f76336add35367db83309dcd06d251c4c2bd2ff9
Erzhan <saralaev03@mail.ru>
2025-01-24 23:07:06 +0500

    Add logo and Welcome text

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -7,6 +7,41 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // logo
+            Icon(
+              Icons.message,
+              size: 60,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+
+            const SizedBox(height: 50),
+
+            // welcome back message
+            Text(
+              "Welcome back, my cute baby!",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 16,
+              ),
+              ),
+
+              const SizedBox(height: 25),
+
+            // email textfield
+            TextField(),
+
+            // pw textfield
+
+            // login button
+
+            // register now
+          ],
+        ),
+      ),
     );
   }
 }
