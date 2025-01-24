eaff0edc09d11362ab0562409e03c8623a87a122
Erzhan <saralaev03@mail.ru>
2025-01-25 00:00:42 +0500

    Add LoginOrRegister page settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:chatapptute/pages/register_page.dart';
+import 'package:chatapptute/auth/login_or_register.dart';
 import 'package:chatapptute/themes/light_mode.dart';
 import 'package:flutter/material.dart';
-import 'pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: RegisterPage(),
+      home: LoginOrRegister(),
       theme: lightMode,
     );
   }
